d114025601d69313376bbb2d92a49f5ce444eaaa
chrismuto <chrismuto@gmail.com>
2022-01-11 15:31:33 -0800

    weather call and geolocation functions linked to weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,54 @@
 var searchInput = document.querySelector("#search-input");
 var submitBtn = document.querySelector("#submitBtn");
 var cityList = document.querySelector("#cityList");
-var newCity;
+var cityDisplay = document.querySelector("#cityDisplay");
+var fiveDayDisplay = document.querySelector("#fiveDayDisplay");
+var newCity = "chicago";
 var newDiv;
 var newA;
+var lat;
+var lon;
 
 //this function takes the name of the last city inputted and adds it to the list along with the API link(?)(replace #placeholder)
 //Store the city data in localStorage so it persists
 //ask John or tutor what information should be stored and retrieved from localStorage(maybe newCity and api return data?)
 function addCity() {
-    newCity = searchInput.value;
+    newCity = "run";
     newDiv = document.createElement("div");
     newA = document.createElement("a");
     newA.setAttribute("class", "text-decoration-none fs-3 text-dark");
     newA.setAttribute("href", "#placeholder");
     newA.textContent = newCity;
+    console.log(newA.textContent);
     cityList.appendChild(newDiv);
     newDiv.appendChild(newA);
     //local storage push goes here
 }
 
-//this function will call the weather api
-function callWeather() {
-
+//this function will call the weather api with the geo api information
+function callWeather(lat, lon) {
+    var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alert&appid=d06d736f70b1c7547ee6d36a7c3c8929&units=imperial";
+    fetch(weatherApiUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
 }
+
+callGeo();
 
 //this function will translate city names into lat/long coordinates for weather api
 function callGeo() {
-
+    var geolocateApi = "https://api.openweathermap.org/geo/1.0/direct?q=" + newCity + "&limit=1&appid=d06d736f70b1c7547ee6d36a7c3c8929";
+    fetch(geolocateApi)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
 }
 
 //this function should push the data from the weather api into new div elements to display
@@ -49,6 +70,6 @@ function updateDisplay() {
 //button should run function to call API and get weather information
 //button should run function to push weather information to main body
 //button should update the display of the div element containing past city searches
-submitBtn.addEventListener("click", function() {
-    addCity();
-})
+// submitBtn.addEventListener("click", function() {
+//     addCity();
+// })
